7cdb65f7379a93d590571c589b5bc7f85e892b03
techwithguga <guramxatiashvili@hotmail.com>
2024-02-05 14:30:40 +0000

    select random int form 1 to 100

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 #choose a number between 1 to 100 
+from random import randint
+
+print("Welcome to the number guess game!")
+print("I'm thinking of a number between 1 and 100")
+answer = randint(1,100)
+
+guess = input("Guess the number: ")    
+
