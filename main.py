cc2a4665cfc6de2d877f1227fa1aae5916834db3
techwithguga <guramxatiashvili@hotmail.com>
2024-02-05 14:32:29 +0000

    check answer function

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 #choose a number between 1 to 100 
 from random import randint
 
+def check_answer(guess,answer):
+    if guess > answer:
+        print('too high')
+    elif guess < answer:
+        print('too low')
+    else:
+        print("you've got it! Correct number was {answer}")
+
+
 print("Welcome to the number guess game!")
 print("I'm thinking of a number between 1 and 100")
 answer = randint(1,100)
