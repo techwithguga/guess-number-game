3c5bba21655b651293c0f5268caeebe0239a6bfd
techwithguga <guramxatiashvili@hotmail.com>
2024-02-05 14:38:05 +0000

    set difficulty and corresponding tries

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 #choose a number between 1 to 100 
 from random import randint
 
+EASY_LEVEL_TRIES = 10
+HARD_LEVEL_TRIES = 5
+
+#check user answer against actuan chosen number
 def check_answer(guess,answer):
+    
     if guess > answer:
         print('too high')
     elif guess < answer:
@@ -9,10 +14,19 @@ def check_answer(guess,answer):
     else:
         print("you've got it! Correct number was {answer}")
 
+#function to set difficulty
+def set_difficulty():
+    level = input("Choose a difficulty: Type 'easy' or 'hard' :")
+    if level == "easy":
+        return EASY_LEVEL_TRIES
+    else:
+        return HARD_LEVEL_TRIES
+        
 
 print("Welcome to the number guess game!")
 print("I'm thinking of a number between 1 and 100")
 answer = randint(1,100)
 
 guess = input("Guess the number: ")    
+tries =  set_difficulty()
 
