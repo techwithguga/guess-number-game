39f62dac2003b293d9465aa75fca5565137592ca
techwithguga <guramxatiashvili@hotmail.com>
2024-02-06 15:55:12 +0000

    repeat guess game if user inputs wrong answer

--- main.py
+++ main.py
@@ -22,13 +22,21 @@ def set_difficulty():
     else:
         return HARD_LEVEL_TRIES
         
+def game():
+    print("Welcome to the number guess game!")
+    print("I'm thinking of a number between 1 and 100")
+    answer = randint(1,100)
+    print(f'the answer is : {answer}')
+    
+    tries =  set_difficulty()
+    print(f'You have {tries} remaining to guess the number.')
+    
+    #repeat guessing if they get it wrong and have tries left
+    guess = 0
+    while guess != answer:
+    
+        guess = int(input("Guess the number: ")) 
+        check_answer(guess, answer)
 
-print("Welcome to the number guess game!")
-print("I'm thinking of a number between 1 and 100")
-answer = randint(1,100)
-print(f'the answer is : {answer}')
-tries =  set_difficulty()
-print(f'You have {tries} remaining to guess the number.')
+game()
 
-guess = int(input("Guess the number: ")) 
-check_answer(guess, answer)
